151dafa906990db3c32c3345abc90a59a3bc2113
NielsvandenDries <niels@openmindedit.nl>
2023-08-07 14:06:24 +0200

    update

---
+++ components/Registration.php
@@ -0,0 +1,19 @@
+<?php namespace Nielsvandendries\Blacklistingbot\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Registration extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'registration Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
